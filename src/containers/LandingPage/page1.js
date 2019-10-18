21915642c9d84bb32f55b36c669dea2286edc117
Pondok Programmer <pondokit@Thinkpad-X200s>
2019-10-18 20:55:26 +0700

    update some layout

--- src/containers/LandingPage/page1.js
+++ src/containers/LandingPage/page1.js
@@ -5,14 +5,14 @@ const Page1 = () => {
   return (
     <>
       <div className="page1">
-        <h1>
+        <span className="h1">
           <div>Share your</div>
           <div className="anim-2">beautiful</div>
           <div>world with</div>
           <div>
             <Logo />
           </div>
-        </h1>
+        </span>
       </div>
     </>
   );
